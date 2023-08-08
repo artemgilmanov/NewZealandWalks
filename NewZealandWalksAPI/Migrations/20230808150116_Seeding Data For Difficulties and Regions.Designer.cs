60d0decf87d2a1886b9616d9ce7a3c3841e92a63
Artem Gilmanov <artemapochta@gmail.com>
2023-08-08 17:02:57 +0200

    Seed start data in DBContext; Created new migration;

--- NewZealandWalksAPI/Migrations/20230808150116_Seeding Data For Difficulties and Regions.Designer.cs
+++ NewZealandWalksAPI/Migrations/20230808150116_Seeding Data For Difficulties and Regions.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NewZealandWalksAPI.Data;
 
@@ -11,9 +12,11 @@ using NewZealandWalksAPI.Data;
 namespace NewZealandWalksAPI.Migrations
 {
     [DbContext(typeof(NZWalksDbContext))]
-    partial class NZWalksDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230808150116_Seeding Data For Difficulties and Regions")]
+    partial class SeedingDataForDifficultiesandRegions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
