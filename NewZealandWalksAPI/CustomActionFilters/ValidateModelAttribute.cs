7d85de164819d9eb98285246dde602865419ff24
Artem Gilmanov <artemapochta@gmail.com>
2023-08-09 11:16:13 +0200

    merged validation to master

--- NewZealandWalksAPI/CustomActionFilters/ValidateModelAttribute.cs
+++ NewZealandWalksAPI/CustomActionFilters/ValidateModelAttribute.cs
@@ -7,7 +7,7 @@ namespace NewZealandWalksAPI.CustomActionFilters
     {
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            if(context.ModelState.IsValid == false)
+            if (context.ModelState.IsValid == false)
             {
                 context.Result = new BadRequestResult();
             }
